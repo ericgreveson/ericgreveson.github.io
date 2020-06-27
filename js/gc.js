6c7fc6c14cc85867cbe3c06434ad185203f3f523
Eric Greveson <eric@greveson.co.uk>
2020-06-27 16:01:35 +0100

    Move old GC website across to Github Pages

---
+++ js/gc.js
@@ -0,0 +1,53 @@
+﻿// Page onload handler
+function gcOnLoad()
+{
+  // Add event handlers
+  gcMakeButton( 'servicesButton', 'services' );
+  gcMakeButton( 'contactButton', 'contact' );
+  gcMakeButton( 'aboutButton', 'about' );
+}
+
+function gcMakeButton( id, page )
+{
+  $(id).observe( 'mouseover', gcMouseOver );
+  $(id).observe( 'mouseout', gcMouseOut );
+
+  // Pre-cache images
+  preloadImages( $(id).src );
+}
+
+// pre-cache rollover images
+function preloadImages( src )
+{
+  var d = document;
+  if ( d.images )
+  {
+    if ( !d.gcCache ) d.gcCache = new Object();
+    var im = new Image();
+    im.src = src;
+    var imOn = new Image();
+    imOn.src = src.replace( ".png", "_on.png" );
+    d.gcCache[ im.src ] = im;
+    d.gcCache[ im.src + " on" ] = imOn;
+  }
+}
+
+function gcMouseOver( event )
+{
+  var im = event.element();
+  var isOn = im.src.search( "_on.png" ) != -1;
+  if ( !isOn )
+  {
+    im.src = im.src.replace( ".png", "_on.png" );
+  }
+}
+
+function gcMouseOut( event )
+{
+  var im = event.element();
+  var isOn = im.src.search( "_on.png" ) != -1;
+  if ( isOn )
+  {
+    im.src = im.src.replace( "_on.png", ".png" );
+  }
+}
