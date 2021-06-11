9d5d0075dad16fca500e961a8dc3dbc578eec20b
Eric Greveson <egreveson@brainomix.com>
2021-06-11 22:49:53 +0100

    Separate data from presentation, add a couple of cars. Welcome to horrific bloaty modern JS horror

---
+++ cars/cars.js
@@ -0,0 +1,52 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    cars: [
+      {
+        name: "Ferrari 456M GT",
+        image: "ferrari_456_1.jpg",
+        link: "https://www.flickr.com/photos/58441544@N00/27113724462/"
+      },
+      {
+        name: "Lotus Evora",
+        image: "lotus_evora_1.jpg",
+        link: "https://www.flickr.com/photos/harry_nl/14743944453"
+      },
+      {
+        name: "Nissan GT-R",
+        image: "nissan_gtr_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:Nissan_GTR.jpg"
+      },
+      {
+        name: "BMW i8",
+        image: "bmw_i8_1.jpg",
+        link: "https://www.flickr.com/photos/artvlive/47659077282"
+      },
+      {
+        name: "Nissan 370Z",
+        image: "nissan_370z_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:Nissan_370Z_Z34_Coupe_Pack_Automatik_2013_Magma_Red_Heckansicht_seitlich_Flugplatz_Werneuchen.jpg"
+      },
+      {
+        name: "Caterham R300",
+        image: "caterham_r300_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:Caterham_R300.jpg"
+      },
+      {
+        name: "Porsche Boxster (981)",
+        image: "porsche_boxster_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:Porsche_Boxster_-_Type_981_-_China_2015-04-13.jpg"
+      },
+      {
+        name: "Cadillac Deville",
+        image: "cadillac_deville_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:1955_Cadillac_Coupe_deVille_(6997648062).jpg"
+      },
+      {
+        name: "Ariel Atom",
+        image: "ariel_atom_1.jpg",
+        link: "https://commons.wikimedia.org/wiki/File:ArielAtomGoodwood.jpg"
+      },
+    ]
+  }
+});
